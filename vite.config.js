55d2521f96fb85e22d4e44642a91de98cb039fbf
Viktor Chernikov <viktor.chernikov@onet.pl>
2024-02-24 02:28:15 +0100

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/todo/",
   plugins: [react()],
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  }
 })
