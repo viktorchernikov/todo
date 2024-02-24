73f0ced98441057687f53b2e6e06bc8a6746d7d9
Viktor Chernikov <viktor.chernikov@onet.pl>
2024-02-24 12:48:44 +0100

    Basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import TaskListBlob from "./components/TaskListBlob"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header id="site-header" class="container-fluid d-flex justify-content-between align-items-center px-4 py-3 shadow-sm">
+          <h1>todo.</h1>
+          <p class="mb-0">Autor: Viktor Chernikov</p>
+      </header>
+      <main id="site-content-wrapper" class='d-flex justify-content-center'>
+        <div class='container-md px-4 py-4'>
+          <div class="input-group mb-4">
+            <input type="text" class="form-control px-4 py-3" placeholder="What do you need to do?" aria-label="What do you need to do?" aria-describedby="controls-addtask"/>
+            <button id="controls-addtask" class="btn btn-primary px-4" type="button">+</button>
+          </div>
+          <section id="tasklist-container" class="py-2 rounded-3 shadow-sm">
+            <TaskListBlob title="Wiem że nie chcę się już dłużej bać"/>
+            <TaskListBlob title="Nie chcę tańczyć do melodii którą znam"/>
+            <TaskListBlob title="Jestem wolny już mnie porwał wiatr"/>
+          </section>
+        </div>
+      </main>
     </>
   )
 }
