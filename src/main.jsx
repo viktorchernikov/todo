0413b364823086b12cff98c450349b14b6c7851f
Viktor Chernikov <viktor.chernikov@onet.pl>
2024-02-24 02:29:02 +0100

    Bootstrap import, organized styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import * as bootstrap from 'bootstrap'
+import './scss/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
